546be074f6de1dfc877cc8287ddc781e2dd102d8
Botond Orban <orbanbotond81@gmail.com>
2023-12-05 16:59:01 +0100

    test commit

---
+++ JavaScript/JavaScript/Kalkulátor/Kalkulator.js
@@ -0,0 +1,7 @@
+function fontKilogramkonverter(valNUM) {
+    document.getElementById("outputkilogramm").innerHTML = valNUM * 0.4536;
+}
+
+function Kilogramfontkonverter(valNUM) {
+    document.getElementById("outputFont").innerHTML = valNUM / 0.4536;
+}
